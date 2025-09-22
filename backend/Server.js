1cba8ead1e7b85e3aa22c6b4a9e3b665c8c0cdf4
Victor Gabriel <Vicgabri75@gmail.com>
2025-09-22 15:32:08 -0300

    Adiciona geração de QR Code e Ajustes no Professor.html

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const QRCode = require("qrcode");
 
 app.use(cors());
 app.use(express.json());
@@ -19,9 +20,21 @@ app.post("/criar-lista", (req, res) => {
   listaAberta = true;
   expiresAt = Date.now() + (parseInt(duracao) * 60 * 60 * 1000);
 
-  res.json({ msg: `Lista criada com limite de ${limite} alunos e duração de ${duracao} horas.` });
-});
 
+  const alunoUrl = "https://ahujgn-ip-167-249-108-155.tunnelmole.net/aluno.html";
+
+  QRCode.toDataURL(alunoUrl, (err, qrCodeData) => {
+    if (err) {
+      console.error("Erro ao gerar QR Code", err);
+      return res.status(500).json({ msg: "Erro ao gerar QR Code" });
+      
+    }
+
+  res.json({ msg: `Lista criada com limite de ${limite} alunos e duração de ${duracao} horas.` ,
+  qrCode: qrCodeData});
+  
+});
+});
 // Fechar lista
 app.post("/fechar-lista", (req, res) => {
   listaAberta = false;
@@ -78,4 +91,4 @@ app.listen(3000, () => console.log("http://localhost:3000/"));
 // Certifique-se de ter o Express instalado: npm install express cors
 // Acesse o frontend em: http://localhost:3000/Professor.html
 // Acesse o frontend do aluno em: http://localhost:3000/Aluno.html
-// Use ferramentas como Postman ou Insomnia para testar as rotas POST.
+// Use ferramentas como Postman ou Insomnia para testar as rotas POST.no
